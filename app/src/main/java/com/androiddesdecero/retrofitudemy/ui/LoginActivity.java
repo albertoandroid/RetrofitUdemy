c2c916f4bfeb58b4a5bb84490f16f1639d667026
Alberto <info@androiddesdecero.com>
2018-12-01 14:47:48 +0100

    08 LoginActivity

--- app/src/main/java/com/androiddesdecero/retrofitudemy/ui/LoginActivity.java
+++ app/src/main/java/com/androiddesdecero/retrofitudemy/ui/LoginActivity.java
@@ -2,14 +2,62 @@ package com.androiddesdecero.retrofitudemy.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.androiddesdecero.retrofitudemy.R;
+import com.androiddesdecero.retrofitudemy.model.Profesor;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etPassword;
+    private EditText etEmail;
+    private Button btLogin;
+    private TextView tvSignUp;
+
+    private Profesor profesor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setUpView();
+    }
+
+    private void setUpView(){
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            etEmail.setError(getResources().getString(R.string.email_error));
+            etEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError(getResources().getString(R.string.email_doesnt_match));
+            etEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            etPassword.setError(getResources().getString(R.string.password_error));
+            etPassword.requestFocus();
+            return;
+        }
+
+        if(password.length()<4){
+            etPassword.setError(getResources().getString(R.string.password_error_less_than));
+            etPassword.requestFocus();
+            return;
+        }
+
+        profesor = new Profesor();
+        profesor.setEmail(email);
+        profesor.setPassword(password);
+        login();
     }
 }
