40e1e6896c3fbdd99d4cceda01ec4bcef3593bd6
Alberto <info@androiddesdecero.com>
2018-12-01 12:37:14 +0100

    03 Sign up XML

--- app/src/main/java/com/androiddesdecero/retrofitudemy/SignUpActivity.java
+++ app/src/main/java/com/androiddesdecero/retrofitudemy/SignUpActivity.java
@@ -3,11 +3,11 @@ package com.androiddesdecero.retrofitudemy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 }
