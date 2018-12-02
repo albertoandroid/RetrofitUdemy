b386ab3cbd5dd079d99893809a654b6e790c1d78
Alberto <info@androiddesdecero.com>
2018-12-02 17:18:19 +0100

    33 Relacion Muchos a muchos

---
+++ app/src/main/java/com/androiddesdecero/retrofitudemy/model/Lenguaje.java
@@ -0,0 +1,27 @@
+package com.androiddesdecero.retrofitudemy.model;
+
+/**
+ * Created by albertopalomarrobledo on 2/12/18.
+ */
+
+public class Lenguaje {
+
+    private Long id;
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ app/src/main/java/com/androiddesdecero/retrofitudemy/model/ProfesorLenguaje.java
@@ -0,0 +1,27 @@
+package com.androiddesdecero.retrofitudemy.model;
+
+/**
+ * Created by albertopalomarrobledo on 2/12/18.
+ */
+
+public class ProfesorLenguaje {
+
+    private Profesor profesor;
+    private Lenguaje lenguaje;
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public Lenguaje getLenguaje() {
+        return lenguaje;
+    }
+
+    public void setLenguaje(Lenguaje lenguaje) {
+        this.lenguaje = lenguaje;
+    }
+}
