23130635c38f748488f71fdaa369814c6e1034b6
Alberto <info@androiddesdecero.com>
2018-12-02 19:08:53 +0100

    Login Intercertor

--- app/src/main/java/com/androiddesdecero/retrofitudemy/api/WebService.java
+++ app/src/main/java/com/androiddesdecero/retrofitudemy/api/WebService.java
@@ -1,5 +1,7 @@
 package com.androiddesdecero.retrofitudemy.api;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,10 +14,15 @@ public class WebService {
     private static final String BASE_URL = "http://10.0.2.2:8040/";
     private static WebService instance;
     private Retrofit retrofit;
+    private HttpLoggingInterceptor loggingInterceptor;
+    private OkHttpClient.Builder httpClientBuilder;
 
     private WebService(){
+        loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClientBuilder = new OkHttpClient.Builder().addInterceptor(loggingInterceptor);
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(httpClientBuilder.build())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
